41f1c42ccaffddd07671ad6ef5e759a7897ceeb1
stashymane <me@stashy.dev>
2026-08-30 20:09:03 +0300

    fix publishing again

--- kotlin/build-logic/src/main/kotlin/InfersMavenPublishPlugin.kt
+++ kotlin/build-logic/src/main/kotlin/InfersMavenPublishPlugin.kt
@@ -1,6 +1,6 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.credentials.PasswordCredentials
+import org.gradle.api.artifacts.repositories.PasswordCredentials
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.credentials
@@ -18,10 +18,10 @@ class InfersMavenPublishPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.pluginManager.apply("com.vanniktech.maven.publish")
 
-        val repo: String = System.getenv("GITHUB_REPOSITORY") ?: return
-
         target.extensions.configure<PublishingExtension> {
             repositories.maven {
+                val repo = target.providers.environmentVariable("GITHUB_REPOSITORY").getOrElse("stashymane/infe.rs")
+
                 name = "GitHubPackages"
                 url = target.uri("https://maven.pkg.github.com/$repo")
 
